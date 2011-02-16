3fcc9f11cc90feba6c25d69d5fa76fcd35c2151c
Neil Gupta <neil@tabuleapp.com>
2011-02-16 11:37:00 -0600

    Initial Commit

---
+++ RSSReader/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RSSReader
+//
+//  Created by Neil Gupta on 2/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
